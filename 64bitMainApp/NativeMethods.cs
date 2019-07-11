5d3007976da28ae1090465966918188d7b75f1c4
Idearum <Idearum@users.noreply.github.com>
2019-07-11 14:32:29 +0200

    Update NativeMethods.cs

--- 64bitMainApp/NativeMethods.cs
+++ 64bitMainApp/NativeMethods.cs
@@ -79,19 +79,6 @@ namespace AltInjector
                 Process targetProcess = Process.GetProcessById(processID);
                 string DocumentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
                 logger.Info("Target process name: {processName}", targetProcess.ProcessName);
-
-                /*
-                using (System.IO.StreamWriter file = new System.IO.StreamWriter(DocumentsPath + "\\My Mods\\SpecialK\\Global\\whitelist.ini"))
-                {
-                    foreach (string line in lines)
-                    {
-                        // If the line doesn't contain the word 'Second', write the line to the file.
-                        if (!line.Contains("Second"))
-                        {
-                            file.WriteLine(line);
-                        }
-                    }
-                }*/
                 
                 bool isBlacklisted = Array.Exists(blacklist, x => x == targetProcess.ProcessName);
                 if (isBlacklisted == false)
