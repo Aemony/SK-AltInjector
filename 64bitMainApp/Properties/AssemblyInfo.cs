ade6a3673a8d1df743686ea45ab8e53d9a875bcd
Idearum <Idearum@users.noreply.github.com>
2019-07-14 17:31:05 +0200

    ClickOnce: Undo the latest test

--- 64bitMainApp/Properties/AssemblyInfo.cs
+++ 64bitMainApp/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SK-TinyInjector")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Companion injector for Special K")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SK-TinyInjector")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright © 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.6.*")]
-[assembly: AssemblyFileVersion("1.6.*")]
+[assembly: AssemblyVersion("1.6.0.18")]
+[assembly: AssemblyFileVersion("1.6.0.18")]
